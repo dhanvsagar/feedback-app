5dd3d84a3423df29cc8dcf816548e0c70dbf1549
dhanvsagar <dhanvsagar@gmail.com>
2025-06-01 10:55:10 -0400

    Add Feedback app Initial draft

--- app/feedback-app/main.py
+++ app/feedback-app/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/status")
 def status():
     return {"Status": "OK"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(name="index.html", request=request)
